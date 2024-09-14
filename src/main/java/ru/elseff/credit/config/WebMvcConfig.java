c9a39a3f72ea00008fe0e2010861a9905193857e
elseff <danilapetrov522@gmail.com>
2024-09-14 21:27:48 +0400

    add allowed origins env property

--- src/main/java/ru/elseff/credit/config/WebMvcConfig.java
+++ src/main/java/ru/elseff/credit/config/WebMvcConfig.java
@@ -1,20 +1,29 @@
 package ru.elseff.credit.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("#{'${allowed.origins}'.split(' ')}")
+    List<String> origins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        String origin = "http://localhost:4200";
-        registry.addMapping("/**")
-                .allowedOrigins("http://192.168.100.4:4200")
-                .allowedOrigins(origin)
-                .allowedMethods("*");
-        log.info("Allowed origin --- " + origin);
+        origins.forEach(
+                origin -> {
+                    registry.addMapping("/**")
+                            .allowedOrigins(origin)
+                            .allowedMethods("*");
+                    log.info("Allowed origin --- " + origin);
+                }
+        );
     }
 }
