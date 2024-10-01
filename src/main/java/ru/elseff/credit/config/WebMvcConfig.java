d22f0bbe76453c9de290ef34be9d65c96fdcf48e
elseff <danilapetrov522@gmail.com>
2024-10-01 13:35:48 +0400

    return cors origins

--- src/main/java/ru/elseff/credit/config/WebMvcConfig.java
+++ src/main/java/ru/elseff/credit/config/WebMvcConfig.java
@@ -17,14 +17,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        origins.forEach(
-//                origin -> {
-//                    registry.addMapping("/**")
-//                            .allowedOrigins(origin)
-//                            .allowedMethods("*");
-//                    log.info("Allowed origin --- " + origin);
-//                }
-//        );
-        registry.addMapping("/**").allowedMethods("*");
+        origins.forEach(
+                origin -> {
+                    registry.addMapping("/**")
+                            .allowedOrigins(origin)
+                            .allowedMethods("*");
+                    log.info("Allowed origin --- " + origin);
+                }
+        );
     }
 }
