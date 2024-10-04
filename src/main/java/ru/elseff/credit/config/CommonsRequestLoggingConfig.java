ebb374b8fb10cd4fe1de6f8ed3bc72f737bf6b72
elseff <danilapetrov522@gmail.com>
2024-10-04 11:23:31 +0400

    logging incoming requests

---
+++ src/main/java/ru/elseff/credit/config/CommonsRequestLoggingConfig.java
@@ -0,0 +1,19 @@
+package ru.elseff.credit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class CommonsRequestLoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+
+        return loggingFilter;
+    }
+}
